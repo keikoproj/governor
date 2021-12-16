af3fc46671b5669703b991c0d5483a7950ca647f
xshao <Sheldon_Shao@intuit.com>
2021-12-15 21:57:49 -0800

    Add reason metrics
    Signed-off-by: xshao <Sheldon_Shao@intuit.com>
    Signed-off-by: shaoxt <shaoxt@gmail.com>

--- pkg/reaper/common/metrics.go
+++ pkg/reaper/common/metrics.go
@@ -1,12 +1,7 @@
 package common
 
-import (
-	"context"
-)
-
-// API for Query Metrics
 type MetricsAPI interface {
 
 	// Set Metric value on metric
-	SetMetricValue(ctx context.Context, metricName string, tags map[string]string, value float64) error
+	SetMetricValue(metricName string, tags map[string]string, value float64) error
 }
