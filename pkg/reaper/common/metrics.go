1e9a71fac20758c837aae37d165db3fb513aee85
Sheldon Shao <shaoxt@gmail.com>
2021-12-20 17:33:09 -0800

    Fix merge issue
    Fix merge issue

--- pkg/reaper/common/metrics.go
+++ pkg/reaper/common/metrics.go
@@ -1,26 +1,6 @@
 package common
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 type MetricsAPI interface {
-
 	// Set Metric value on metric
 	SetMetricValue(metricName string, tags map[string]string, value float64) error
-=======
-import (
-	"context"
-)
-
-// API for Query Metrics
-type MetricsAPI interface {
-
-	// Set Metric value on metric
-	SetMetricValue(ctx context.Context, metricName string, tags map[string]string, value float64) error
->>>>>>> 7a298b8 (Pushgateway API)
-=======
-type MetricsAPI interface {
-
-	// Set Metric value on metric
-	SetMetricValue(metricName string, tags map[string]string, value float64) error
->>>>>>> 25d4af5 (Add reason metrics)
 }
