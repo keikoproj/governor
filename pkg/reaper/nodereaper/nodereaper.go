82832f9970cffb359e06dc22b8f551d23eafe400
Terence Ho <terence.ho@tasktop.com>
2022-10-28 18:29:13 -0700

    Add capability to reap EC2 nodes not managed by ASG
    Signed-off-by: Terence Ho <terence.ho@tasktop.com>

--- pkg/reaper/nodereaper/nodereaper.go
+++ pkg/reaper/nodereaper/nodereaper.go
@@ -563,7 +563,7 @@ func (ctx *ReaperContext) reapOldNodes(w ReaperAwsAuth) error {
 
 		if !ctx.DryRun {
 			log.Infof("reaping old node %v -> %v", instance.NodeName, instance.InstanceID)
-			err = ctx.terminateInstance(w.ASG, instance.InstanceID, instance.NodeName)
+			err = ctx.terminateInstance(w, instance.InstanceID, instance.NodeName)
 			if err != nil {
 				return err
 			}
@@ -623,7 +623,7 @@ func (ctx *ReaperContext) reapUnhealthyNodes(w ReaperAwsAuth) error {
 		if !ctx.DryRun {
 			log.Infof("reaping unhealthy node %v -> %v", instance.NodeName, instance)
 
-			err = ctx.terminateInstance(w.ASG, instance.InstanceID, instance.NodeName)
+			err = ctx.terminateInstance(w, instance.InstanceID, instance.NodeName)
 			if err != nil {
 				return err
 			}
